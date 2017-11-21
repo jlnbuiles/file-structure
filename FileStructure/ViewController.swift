d72e6c96233fafd64537cb956e2beab13c65f2e2
Julian Builes <jlnbuiles@gmail.com>
2017-11-21 17:40:09 -0500

    Initial Commit

---
+++ FileStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FileStructure
+//
+//  Created by Julian Builes on 11/21/17.
+//  Copyright © 2017 Julian Builes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
